5ec4cd29448d3a579637bcf159c0709150f4d66e
thesr5 <thesr5@gmail.com>
2014-05-13 14:41:15 -0700

    Config File

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+$bnick = "TFBot";
+$authname = "AUTHNAME";
+$authpass = "AUTHPASS";
+$jchan = "#teamewix"; //channel to join
+$socket = fsockopen("irc.quakenet.org",6667); //network to connect to
+$cmdsym = "!"; //command prefix (ex:!commands)
+$pugmax = 12; //default player count needed to start pug can be changed as a admin
+
+?>
